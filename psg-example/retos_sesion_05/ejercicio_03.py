18481d2cfa92589bf2fd9b2f95a6b19ee3076122
DrakmerAlain76 <56520535+DrakmerAlain76@users.noreply.github.com>
2025-05-24 14:49:35 -0400

    retos_sesion_05

---
+++ psg-example/retos_sesion_05/ejercicio_03.py
@@ -0,0 +1,21 @@
+#Escribe un programa en Python que convierta 1000000 de segundos en semanas, días, horas, minutos y segundos
+#Ejemplo:
+#788521 segundos = 9 días 3 horas 2 minutos 1 segundo
+# Total de segundos
+total_segundos = 1000000
+
+# Cálculos
+semanas = total_segundos // (7 * 24 * 60 * 60)
+resto = total_segundos % (7 * 24 * 60 * 60)
+
+días = resto // (24 * 60 * 60)
+resto %= (24 * 60 * 60)
+
+horas = resto // (60 * 60)
+resto %= (60 * 60)
+
+minutos = resto // 60
+segundos = resto % 60
+
+# Mostrar el resultado
+print(f"{total_segundos} segundos = {semanas} semanas {días} días {horas} horas {minutos} minutos {segundos} segundos")
