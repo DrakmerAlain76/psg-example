90f0e7ee280c33389022fe1a028e9f45eaf304b4
DrakmerAlain76 <56520535+DrakmerAlain76@users.noreply.github.com>
2025-06-07 14:45:44 -0400

    retos_sesion_07

---
+++ psg-example/retos_sesion_07/ejercicio_01.py
@@ -0,0 +1,4 @@
+# Escribir una cadena con una frase de una libro o un poema
+poema = "Cuando el mar sea redondo y el sol deje de brillar, ese será el día en que te pueda olvidar."
+
+print(poema)
---
+++ psg-example/retos_sesion_07/ejercicio_06.py
@@ -0,0 +1,19 @@
+# 6 Agregar 5 Ejemplos con otras funciones no vistas en la sesión
+# Utilizar la documentación Métodos de cadenas
+
+texto = "Python es divertido"
+print(texto.startswith("Python")) 
+
+archivo = "reporte_final.pdf"
+print(archivo.endswith(".pdf"))  
+
+mensaje = "Hola mundo"
+nuevo_mensaje = mensaje.replace("mundo", "universo")
+print(nuevo_mensaje)  
+
+frase = "La luna es hermosa esta noche"
+posicion = frase.find("hermosa")
+print(posicion)  
+
+titulo = "el señor de los anillos"
+print(titulo.title())  
