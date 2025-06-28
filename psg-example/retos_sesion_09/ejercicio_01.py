775015cbd08d0095e4892ce380868b8db753949e
DrakmerAlain76 <56520535+DrakmerAlain76@users.noreply.github.com>
2025-06-28 14:24:06 -0400

    retos de sesion 09

---
+++ psg-example/retos_sesion_09/ejercicio_01.py
@@ -0,0 +1,15 @@
+# Crear una lista con 5 números flotantes y 5 enteros y mostrar todos los elementos y sus tipos de datos utilizando sus índices
+
+numeros = [3.14, 2.71, 1.618, 0.577, 4.669, 1, 2, 3, 4, 5]
+
+
+print(f"Índice 0: Valor = {numeros[0]}, Tipo = {type(numeros[0])}")
+print(f"Índice 1: Valor = {numeros[1]}, Tipo = {type(numeros[1])}")
+print(f"Índice 2: Valor = {numeros[2]}, Tipo = {type(numeros[2])}")
+print(f"Índice 3: Valor = {numeros[3]}, Tipo = {type(numeros[3])}")
+print(f"Índice 4: Valor = {numeros[4]}, Tipo = {type(numeros[4])}")
+print(f"Índice 5: Valor = {numeros[5]}, Tipo = {type(numeros[5])}")
+print(f"Índice 6: Valor = {numeros[6]}, Tipo = {type(numeros[6])}")
+print(f"Índice 7: Valor = {numeros[7]}, Tipo = {type(numeros[7])}")
+print(f"Índice 8: Valor = {numeros[8]}, Tipo = {type(numeros[8])}")
+print(f"Índice 9: Valor = {numeros[9]}, Tipo = {type(numeros[9])}")
