f536c9159b8231053e104c876d88a8ed69d42f24
DrakmerAlain76 <56520535+DrakmerAlain76@users.noreply.github.com>
2025-07-05 14:19:30 -0400

    retos de sesion 10

---
+++ psg-example/retos_sesion_10/ejercicio_01.py
@@ -0,0 +1,8 @@
+# Elimina los elementos de oficina repetidos de la cadena
+# 📎📐📏✏️🖊️🖋️📎📌📏📇🗂️📁📌🗃️✏️📂🖇️
+
+conjunto = {x for x in '📎📐📏✏️🖊️🖋️📎📌📏📇🗂️📁📌🗃️✏️📂🖇️'}
+print(conjunto)
+print(type(conjunto))
+
+print("Los elementos de un conjunto no pueden ser duplicados")
